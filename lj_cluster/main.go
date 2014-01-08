2c421e22f8948e0af39343c232b81306f93ce3df
Katherine Crowson <crowsonkb@gmail.com>
2014-01-08 11:16:52 -0800

    Add usage example to package comment

--- lj_cluster/main.go
+++ lj_cluster/main.go
@@ -1,5 +1,5 @@
 // The lj_cluster command line tool performs hierarchical clustering on
-// LiveJournal friends lists.
+// LiveJournal friends lists. For example: "lj_cluster -user=brad".
 package main
 
 import (
